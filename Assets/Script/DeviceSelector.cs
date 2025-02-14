1bdc839649a960475ef8229c608c9a46cf89f8dc
CHICHIBAEBAE <smartb12@naver.com>
2025-02-14 14:46:30 +0900

    [Feat] : 디바이스가 중간에 추가, 해제 될 시 실시간 반영 테스트 완료

--- Assets/Script/DeviceSelector.cs
+++ Assets/Script/DeviceSelector.cs
@@ -5,6 +5,7 @@ using TMPro;
 using System.Collections.Generic;
 using System;
 using Unity.VisualScripting;
+using System.Collections;
 
 public class DeviceSelector : MonoBehaviour
 {
@@ -12,33 +13,40 @@ public class DeviceSelector : MonoBehaviour
 
     public event Action<string> OnDeviceSelected; // 선택된 장치를 다른 컴포넌트에 전달하는 이벤트
 
+    private List<string> cachedDevices = new List<string>();
+    private WaitForSeconds deviceCheckDelay = new WaitForSeconds(1f);
+
     void Start()
     {
         PopulateDeviceDropdown();
+        StartCoroutine(MonitorDeviceChanges());
     }
 
     private void PopulateDeviceDropdown()
     {
+        string[] devices = Microphone.devices;
         if (Microphone.devices.Length == 0)
         {
             Debug.LogWarning("사용 가능한 마이크 디바이스가 없습니다.");
+            deviceDropdown.ClearOptions();
+            cachedDevices = new List<string>();
             return;
         }
 
-        List<string> options = new List<string>(Microphone.devices);
+        List<string> options = new List<string>(devices);
 
-        foreach (string device in Microphone.devices)
+        foreach (string device in options)
         {
             Debug.Log("마이크 장치: " + device);
 
         }
 
+        deviceDropdown.onValueChanged.RemoveAllListeners();
         deviceDropdown.ClearOptions();
         deviceDropdown.AddOptions(options);
-
         deviceDropdown.onValueChanged.AddListener(OnDropdownChanged);
 
-        if(deviceDropdown.options.Count > 0)
+        if (deviceDropdown.options.Count > 0)
         {
             OnDropdownChanged(deviceDropdown.value);
         }
@@ -49,5 +57,34 @@ public class DeviceSelector : MonoBehaviour
         string selectedDevice = deviceDropdown.options[value].text;
         Debug.Log("선택된 장치 : " +  selectedDevice);
         OnDeviceSelected?.Invoke(selectedDevice);
+    }
+
+    IEnumerator MonitorDeviceChanges()
+    {
+        while(true)
+        {
+            yield return deviceCheckDelay;
+
+            string[] currentDevices = Microphone.devices;
+
+            if(HasDeviceListChanged(currentDevices))
+            {
+                Debug.Log("디바이스 목록 변경");
+                PopulateDeviceDropdown();
+            }
+        }
+    }
+
+    private bool HasDeviceListChanged(string[] newDevices)
+    {
+        if (newDevices.Length != cachedDevices.Count)
+            return true;
+
+        for (int i = 0; i < newDevices.Length; i++)
+        {
+            if (newDevices[i] != cachedDevices[i])
+                return true;
+        }
+        return false;
     }
 }
